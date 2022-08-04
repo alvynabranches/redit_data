1d342708a769f89473275a91c4c9eb35b4c357f6
Alvyn Abranches <alvynabranches@gmail.com>
2022-08-05 03:40:41 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
+# ENV VARIABLES
 export DATASET="data"
+export TABLE=reddit
+export PROJECT="summ-270612"
+export BUCKET="reddit_data_fine_tuning"
+
 bq mk --dataset ${DATASET?}
 
 
-
-
-export TABLE=reddit
 
 # For all data up to 2019.
 export TABLE_REGEX="^201[5678]_[01][0-9]$"
@@ -23,17 +25,12 @@ echo "${QUERY?}" | bq query \
 
 
 
-
-
-export PROJECT="summ-270612"
-export BUCKET="reddit_data_fine_tuning"
-
 export DATADIR="gs://${BUCKET?}/reddit/$(date +"%Y%m%d")"
 
 # The below uses values of $DATASET and $TABLE set
 # in the previous section.
 
-python3 -m pip install apache_beam
+python3 -m pip install apache_beam google-apitools
 python3 create_data.py \
   --output_dir ${DATADIR?} \
   --reddit_table ${PROJECT?}:${DATASET?}.${TABLE?} \
